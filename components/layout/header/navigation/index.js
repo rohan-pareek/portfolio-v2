ef2176cd25a74f934eee649ce08612a0e5d2a58d
Rohan Pareek <rohaniscoding@gmail.com>
2022-03-26 19:38:51 +0530

    added navigation for mobile

--- components/layout/header/navigation/index.js
+++ components/layout/header/navigation/index.js
@@ -1,8 +1,8 @@
 import React, { useMemo } from 'react';
 import Link from 'next/link';
-import Container from '../../container';
 import { useRouter } from 'next/router';
 import CustomMeta from '../../../meta';
+import css from './Navigation.module.css';
 
 const Navigation = props => {
 
@@ -13,28 +13,33 @@ const Navigation = props => {
             case '/':
                 return {
                     title: 'Rohan Pareek | About',
-                    description: 'Welcome to my digital portfolio. You can access my work, education related details'
+                    description: 'Welcome to my digital portfolio. You can access my work, education related details',
+                    page: 'About'
                 }
             case '/projects':
                 return {
                     title: 'Rohan Pareek | Projects',
-                    description: 'Project related information of Rohan Pareek'
+                    description: 'Project related information of Rohan Pareek',
+                    page: 'Projects'
                 }
             case '/education':
                 return {
                     title: 'Rohan Pareek | Education',
-                    description: 'Education related information of Rohan Pareek'
+                    description: 'Education related information of Rohan Pareek',
+                    page: 'Education'
                 }
             case '/skills':
                 return {
                     title: 'Rohan Pareek | Skills',
-                    description: 'Skills related information of Rohan Pareek'
+                    description: 'Skills related information of Rohan Pareek',
+                    page: 'Skills'
                 }
 
             default:
                 return {
                     title: 'Rohan Pareek',
-                    description: 'Digital Portfolio of Rohan Pareek'
+                    description: 'Digital Portfolio of Rohan Pareek',
+                    page: 'About'
                 }
         }
     }, [asPath]);
@@ -43,7 +48,7 @@ const Navigation = props => {
         <>
             <CustomMeta title={activeMeta.title} description={activeMeta.description} />
             <nav className='py-5'>
-                <ul className='flex justify-between border-gray-200 border-b pb-2'>
+                <ul className='justify-between border-gray-200 border-b pb-2 hidden md:flex'>
                     <li>
                         <Link href="/">
                             <a className={`pb-2 ${'/' === asPath ? 'border-brand-dark border-b-4' : ''}`}>
@@ -73,6 +78,50 @@ const Navigation = props => {
                         </Link>
                     </li>
                 </ul>
+                <section className='flex md:hidden'>
+                    <div className='flex-1 text-lg flex justify-center items-center'>
+                        {activeMeta.page}
+                    </div>
+                    <section className='absolute top-5 right-5'>
+                        <div className='relative'>
+                            <button className={css['menu-button']}>
+                                <i className="fa fa-ellipsis-v"></i>
+                            </button>
+                            <div className={`absolute right-0 top-0 shadow p-2 bg-white px-4 ${css.menu}`}>
+                                <ul>
+                                    <li className='my-1'>
+                                        <Link href="/">
+                                            <a>
+                                                About
+                                            </a>
+                                        </Link>
+                                    </li>
+                                    <li className='my-1'>
+                                        <Link href="/projects">
+                                            <a>
+                                                Projects
+                                            </a>
+                                        </Link>
+                                    </li>
+                                    <li className='my-1'>
+                                        <Link href="/education">
+                                            <a>
+                                                Education
+                                            </a>
+                                        </Link>
+                                    </li>
+                                    <li className='my-1'>
+                                        <Link href="/skills">
+                                            <a>
+                                                Skills
+                                            </a>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+                </section>
             </nav>
         </>
     )
