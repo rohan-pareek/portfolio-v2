cfcf9ea7d3dc5fa43ae0f2a2f5bb94e04a23fc08
Rohan Pareek <rohaniscoding@gmail.com>
2022-04-13 21:19:01 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
+import Script from 'next/script'
 import Layout from '../components/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`} />
+
+      <Script strategy="lazyOnload" id='ga'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GA}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
